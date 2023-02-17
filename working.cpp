d003af2c5fec25212435ad81747988a640c57374
arjunphenom <82772924+arjunphenom@users.noreply.github.com>
2023-02-17 14:36:35 +0530

    Create working.cpp

---
+++ working.cpp
@@ -0,0 +1,14 @@
+// C++ program to illustrate the use
+// of cout object
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	// Print standard output
+	// on the screen
+	cout << "MY SRN IN PES1UG20CS075";
+
+	return 0;
+}
